1e060e0e8a0dedb8538e7b4351ed57e3e52c0cbd
zareenas <zareenas_al@yahoo.co.in>
2019-05-04 20:16:42 +0530

    Added thridclass file and modified secondfile

--- src/com/seleniumlearning/examples/MySecondFile.java
+++ src/com/seleniumlearning/examples/MySecondFile.java
@@ -1,2 +1,13 @@
 //This is new test case
 //This testcase will hceck editing new customer
+//This is line added newly
+package com.seleniumlearning.examples;
+
+public class MySecondFile {
+
+	public static void main(String[] args) {
+		System.out.println("Test secondfile message");
+
+	}
+
+}
---
+++ src/com/seleniumlearning/examples/MyThridClass.java
@@ -0,0 +1,10 @@
+package com.seleniumlearning.examples;
+
+public class MyThridClass {
+
+	public static void main(String[] args) {
+		System.out.println("Test thirdclass message");
+
+	}
+
+}
